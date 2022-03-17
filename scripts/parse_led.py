7a9488b30495f37553fc0708f192f25418014ae4
Patrick Williams <patrick@stwcx.xyz>
2022-03-17 05:03:51 -0500

    action: use PDI type
    Rather than define our own enumeration for the LED Actions,
    reuse the one from Led.Physical's dbus interface.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I4615bed9e87c61d2632fe790bd50892a7f51e2ab

--- scripts/parse_led.py
+++ scripts/parse_led.py
@@ -83,16 +83,13 @@ if __name__ == "__main__":
                 else:
                     priority_dict[led_name] = value
 
-                ofile.write('        {"' + underscore(led_name) + '",')
-                ofile.write(
-                    "phosphor::led::Layout::Action::"
-                    + str(list_dict.get("Action", "Off"))
-                    + ","
-                )
-                ofile.write(str(list_dict.get("DutyOn", 50)) + ",")
-                ofile.write(str(list_dict.get("Period", 0)) + ",")
-                priority = str(list_dict.get("Priority", "Blink"))
-                ofile.write("phosphor::led::Layout::Action::" + priority + ",")
-                ofile.write("},\n")
-            ofile.write("   }},\n")
-        ofile.write("};\n")
+                ofile.write('        {\"' + underscore(led_name) + '\",')
+                ofile.write('phosphor::led::Layout::Action::' +
+                            str(list_dict.get('Action', 'Off')) + ',')
+                ofile.write(str(list_dict.get('DutyOn', 50)) + ',')
+                ofile.write(str(list_dict.get('Period', 0)) + ',')
+                priority = str(list_dict.get('Priority', 'Blink'))
+                ofile.write('phosphor::led::Layout::Action::' + priority + ',')
+                ofile.write('},\n')
+            ofile.write('   }},\n')
+        ofile.write('};\n')
