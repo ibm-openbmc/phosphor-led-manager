0edd5b753cb720de75a1c88696b8d347c6bf241e
Jinu Joy Thomas <jinu.joy.thomas@in.ibm.com>
2023-11-01 15:53:21 +0530

    Add oem script to default LED (#34)
    Add a script to default the power button LED to Blink state
      update the meson to add this to image
    
    Change-Id: I82c72fe29a84cd7c05aaaf477755facde2fbd42b
    
    Signed-off-by: jinuthomas <jinu.joy.thomas@in.ibm.com>

---
+++ scripts/oem/set-power-button-led-to-default-state.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This shell script initialises Power Button LED to its default state.
+
+# Skip running the script if chassis is powered ON.
+current_chassis_status=$(busctl get-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis CurrentPowerState | cut -d" " -f2)
+
+if [ "${current_chassis_status}" = "\"xyz.openbmc_project.State.Chassis.PowerState.On\"" ]; then
+	    echo "Current chassis power state is , $current_chassis_status . Exit set-power-button-led-to-default-state.sh script successfully without initialising the LEDs"
+	    exit 0
+fi
+
+# Explicitly set Blink as the default state
+busctl set-property xyz.openbmc_project.LED.Controller.pca955x_front_sys_pwron0 /xyz/openbmc_project/led/physical/pca955x_front_sys_pwron0 xyz.openbmc_project.Led.Physical State s "xyz.openbmc_project.Led.Physical.Action.Blink"
